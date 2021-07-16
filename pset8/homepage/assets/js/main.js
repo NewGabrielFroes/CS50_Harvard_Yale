428873c649dae3510bd000486371b7ab5580fb95
Gabriel Froes <gabriel.froes32@gmail.com>
2021-07-16 15:54:18 -0300

    criando a main

--- pset8/homepage/assets/js/main.js
+++ pset8/homepage/assets/js/main.js
@@ -20,6 +20,14 @@ if (navClose){
   })
 }
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 
