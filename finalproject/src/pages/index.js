92f142c1e5fb48644f05eac3635231ce7503e031
Gabriel Froes <gabriel.froes32@gmail.com>
2021-08-25 09:30:42 -0300

    material-ui configuration

--- finalproject/src/pages/index.js
+++ finalproject/src/pages/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
+import { Button } from '@material-ui/core';
 
 export default function Home() {
-  return <div>Clone Youtube</div>
+  return (
+  <div>
+    Clone Youtube
+    <Button variant="outlined" color="secondary">Primary</Button>
+  </div>
+  )
 }
